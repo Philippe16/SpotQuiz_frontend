31c95272953a564a936a5b4432fae253167e16c4
Angeliza Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2022-05-08 12:29:09 +0200

    Add spotify search field - Not yet functional

--- src/components/QuestionForm.jsx
+++ src/components/QuestionForm.jsx
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types'
 // CSS
 import '../css/questionForm.css';
 
+// FontAwesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 const QuestionForm = ({ questionNum }) => {
   return (
@@ -38,7 +43,25 @@ const QuestionForm = ({ questionNum }) => {
 
           <input name={ "question" + questionNum + "op4" } type="text" className="option_input" />
         </div>
-      </div>
+
+        <div className="musicSearch_container">
+          <div className="flexRow musicSearch_title_container">
+            <h3 className="opTitle">Choose the music for this question </h3>
+
+            <div className="musicSearch_title_img_container">
+              <img className="musicSearch_title_img" src="../src/images/spotify_logo/Spotify_Icon_RGB_Green.png" alt="The spotify logo" />
+            </div>
+          </div>
+
+          <div className="flexRow musicSearch_input_container">
+            <input className="musicSearch_input" type="text" placeholder="Search by music title" />
+
+            <button className="musicSearch_btn" type="button">
+              <FontAwesomeIcon icon={ faMagnifyingGlass } className="musicSearch_icon" />
+            </button>
+          </div>
+        </div>
+      </div> {/* .flexRow .options_container END */}
     </div>
   )
 }
