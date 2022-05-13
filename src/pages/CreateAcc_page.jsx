eccac6ea52467a0b35f506aeb35ee0371297df20
Philippe16 <philippe.suenson@hotmail.com>
2022-05-13 14:38:44 +0200

    added conditional rendering for create_account.page

--- src/pages/CreateAcc_page.jsx
+++ src/pages/CreateAcc_page.jsx
@@ -5,51 +5,71 @@ import { useState } from 'react';
 
 
 const CreateAcc_page = props => {
+  
+    // Form input fields
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [retypedEmail, setRetypedEmail] = useState("");
     const [retypedPassword, setRetypedPassword] = useState("");
 
+    // Error variables
+    const [error, setError] = useState(null);
+    const [noUsername, setNoUsername] = useState(null);
+    const [noEmail, setNoEmail] = useState(null);
+    const [noRetypedEmail, setNoRetypedEmail] = useState(null);
+    const [noPassword, setNoPassword] = useState(null);
+    const [noRetypedPassword, setNoRetypedPassword] = useState(null);
+
   const handleSubmit = (e) => {
-   e.preventDefault();
-   checkInput();
-       
+    e.preventDefault();
+    checkInput();    
   }
 
   const checkInput = () => {
+    let containsError = false;
+
+    setNoUsername(null);
+    setNoEmail(null);
+    setNoRetypedEmail(null);
+    setNoPassword(null); 
+    setNoRetypedPassword(null);
+    setError(null);
     
     if (username.trim() === "") {
-      alert('Please enter username');
-      return;
+        containsError = true;
+        setNoUsername('Please enter username');
     }
+    
     //Check for the Email
     if (email.trim() === "") {
-      alert('Please enter email');
-      return;
+        containsError = true;
+        setNoEmail('Please enter email');
     }
 
     if (retypedEmail !== email) {
-        alert('Please enter the same email');
-        return;
+        containsError = true;
+        setNoRetypedEmail('Please enter the same email');
       }
 
 
     //Check for the password
     if (password.trim() === "") {
-        alert('Please enter a password');
-        return;
+        containsError = true;
+        setNoPassword('Please enter a password');
       }
 
     if (retypedPassword !== password) {
-        alert('Please enter the same password');
-        return;
+        containsError = true;
+        setNoRetypedPassword('Please enter the same password');
     }  
 
-    alert('Success');
+    if(containsError === true){
+        setError("Error, please check your inputs");       
+    }else{
+        alert('Success'); 
+    }
   };
-
-
 
   return (
     <main>
@@ -58,40 +78,74 @@ const CreateAcc_page = props => {
       </div>
       <form onSubmit={e => handleSubmit(e)} id="acc_form">     
           <h1 id="title">Create Account</h1>
-
-          
-
+    
           <div className="formContent">
             <div className="acc_input_container">
-                <label className="acc_label" htmlFor="email">Username:</label>
-                <input name="username" id="username" className="acc_input" type="text" onChange={e => setUsername(e.target.value)} > 
-                </input>
+              <label className="acc_label" htmlFor="email">Username:</label>
+              <input name="username" id="username" className="acc_input" type="text" onChange={e => setUsername(e.target.value)} > 
+              </input>
+
+              {noUsername && (
+                <div className="error_container">
+                  { noUsername }
+                </div>
+              )}
             </div>
 
             <div className="acc_input_container">
-                <label className="acc_label" htmlFor="email">Email:</label>
-                <input name="email" id="email" className="acc_input" type="text" onChange={e => setEmail(e.target.value)} > 
-                </input>
+              <label className="acc_label" htmlFor="email">Email:</label>
+              <input name="email" id="email" className="acc_input" type="text" onChange={e => setEmail(e.target.value)} > 
+              </input>
+              
+              {noEmail && (
+                <div className="error_container">
+                    { noEmail }
+                </div>
+              )}   
             </div>
 
             <div className="acc_input_container">
-                <label className="acc_label" htmlFor="reEmail">Re-type email:</label>
-                <input name="retypedEmail" id="retypedEmail" className="acc_input" type="text" onChange={e => setRetypedEmail(e.target.value)} > 
-                </input>
+              <label className="acc_label" htmlFor="reEmail">Re-type email:</label>
+              <input name="retypedEmail" id="retypedEmail" className="acc_input" type="text" onChange={e => setRetypedEmail(e.target.value)} > 
+              </input>
+
+              {noRetypedEmail && (
+                <div className="error_container">
+                    { noRetypedEmail }
+                </div>
+              )}   
             </div>
 
             <div className="acc_input_container">
-                <label className="acc_label" htmlFor="password">Password:</label>
-                <input name="password" id="password" className="acc_input" type="password" onChange={e => setPassword(e.target.value)} >
-                </input>
+              <label className="acc_label" htmlFor="password">Password:</label>
+              <input name="password" id="password" className="acc_input" type="password" onChange={e => setPassword(e.target.value)} >
+              </input>
+
+              {noPassword && (
+                <div className="error_container">
+                    { noPassword }
+                </div>
+              )}   
             </div> 
 
             <div className="acc_input_container">
-                <label className="acc_label" htmlFor="rePassword">Re-type password:</label>
-                <input name="retypedPassword" id="retypedPassword" className="acc_input" type="password" onChange={e => setRetypedPassword(e.target.value)} >
-                </input>
+              <label className="acc_label" htmlFor="rePassword">Re-type password:</label>
+              <input name="retypedPassword" id="retypedPassword" className="acc_input" type="password" onChange={e => setRetypedPassword(e.target.value)} >
+              </input>
+              
+              {noRetypedPassword && (
+                <div className="error_container">
+                    { noRetypedPassword }
+                </div>
+              )}   
             </div> 
 
+            {error && (
+              <div className="error_container" id="error_message">
+                { error }
+              </div>
+            )}
+          
             <div id="acc_button_container">  
               <button id="acc_button">Join</button>
             </div>    
@@ -105,7 +159,7 @@ const CreateAcc_page = props => {
                 Go to login
               </Link>
             </div>
-          </div> 
+          </div>
       </form>
     </main>
   )
